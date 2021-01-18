6df16e85b60cf6378b3818fb6e6b808901e33b65
patrickchow0803 <patrickchow0803@gmail.com>
2021-01-18 12:02:15 -0500

    Added an union search covid failure class

---
+++ lib/domain/search/search_covid_failure.freezed.dart
@@ -0,0 +1,254 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies
+
+part of 'search_covid_failure.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+/// @nodoc
+class _$SearchCovidFailureTearOff {
+  const _$SearchCovidFailureTearOff();
+
+// ignore: unused_element
+  _Unexpected unexpected() {
+    return const _Unexpected();
+  }
+
+// ignore: unused_element
+  _unableToLocate unableToLocate() {
+    return const _unableToLocate();
+  }
+}
+
+/// @nodoc
+// ignore: unused_element
+const $SearchCovidFailure = _$SearchCovidFailureTearOff();
+
+/// @nodoc
+mixin _$SearchCovidFailure {
+  @optionalTypeArgs
+  TResult when<TResult extends Object>({
+    @required TResult unexpected(),
+    @required TResult unableToLocate(),
+  });
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object>({
+    TResult unexpected(),
+    TResult unableToLocate(),
+    @required TResult orElse(),
+  });
+  @optionalTypeArgs
+  TResult map<TResult extends Object>({
+    @required TResult unexpected(_Unexpected value),
+    @required TResult unableToLocate(_unableToLocate value),
+  });
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object>({
+    TResult unexpected(_Unexpected value),
+    TResult unableToLocate(_unableToLocate value),
+    @required TResult orElse(),
+  });
+}
+
+/// @nodoc
+abstract class $SearchCovidFailureCopyWith<$Res> {
+  factory $SearchCovidFailureCopyWith(
+          SearchCovidFailure value, $Res Function(SearchCovidFailure) then) =
+      _$SearchCovidFailureCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class _$SearchCovidFailureCopyWithImpl<$Res>
+    implements $SearchCovidFailureCopyWith<$Res> {
+  _$SearchCovidFailureCopyWithImpl(this._value, this._then);
+
+  final SearchCovidFailure _value;
+  // ignore: unused_field
+  final $Res Function(SearchCovidFailure) _then;
+}
+
+/// @nodoc
+abstract class _$UnexpectedCopyWith<$Res> {
+  factory _$UnexpectedCopyWith(
+          _Unexpected value, $Res Function(_Unexpected) then) =
+      __$UnexpectedCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$UnexpectedCopyWithImpl<$Res>
+    extends _$SearchCovidFailureCopyWithImpl<$Res>
+    implements _$UnexpectedCopyWith<$Res> {
+  __$UnexpectedCopyWithImpl(
+      _Unexpected _value, $Res Function(_Unexpected) _then)
+      : super(_value, (v) => _then(v as _Unexpected));
+
+  @override
+  _Unexpected get _value => super._value as _Unexpected;
+}
+
+/// @nodoc
+class _$_Unexpected implements _Unexpected {
+  const _$_Unexpected();
+
+  @override
+  String toString() {
+    return 'SearchCovidFailure.unexpected()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) || (other is _Unexpected);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object>({
+    @required TResult unexpected(),
+    @required TResult unableToLocate(),
+  }) {
+    assert(unexpected != null);
+    assert(unableToLocate != null);
+    return unexpected();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object>({
+    TResult unexpected(),
+    TResult unableToLocate(),
+    @required TResult orElse(),
+  }) {
+    assert(orElse != null);
+    if (unexpected != null) {
+      return unexpected();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object>({
+    @required TResult unexpected(_Unexpected value),
+    @required TResult unableToLocate(_unableToLocate value),
+  }) {
+    assert(unexpected != null);
+    assert(unableToLocate != null);
+    return unexpected(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object>({
+    TResult unexpected(_Unexpected value),
+    TResult unableToLocate(_unableToLocate value),
+    @required TResult orElse(),
+  }) {
+    assert(orElse != null);
+    if (unexpected != null) {
+      return unexpected(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _Unexpected implements SearchCovidFailure {
+  const factory _Unexpected() = _$_Unexpected;
+}
+
+/// @nodoc
+abstract class _$unableToLocateCopyWith<$Res> {
+  factory _$unableToLocateCopyWith(
+          _unableToLocate value, $Res Function(_unableToLocate) then) =
+      __$unableToLocateCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$unableToLocateCopyWithImpl<$Res>
+    extends _$SearchCovidFailureCopyWithImpl<$Res>
+    implements _$unableToLocateCopyWith<$Res> {
+  __$unableToLocateCopyWithImpl(
+      _unableToLocate _value, $Res Function(_unableToLocate) _then)
+      : super(_value, (v) => _then(v as _unableToLocate));
+
+  @override
+  _unableToLocate get _value => super._value as _unableToLocate;
+}
+
+/// @nodoc
+class _$_unableToLocate implements _unableToLocate {
+  const _$_unableToLocate();
+
+  @override
+  String toString() {
+    return 'SearchCovidFailure.unableToLocate()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) || (other is _unableToLocate);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object>({
+    @required TResult unexpected(),
+    @required TResult unableToLocate(),
+  }) {
+    assert(unexpected != null);
+    assert(unableToLocate != null);
+    return unableToLocate();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object>({
+    TResult unexpected(),
+    TResult unableToLocate(),
+    @required TResult orElse(),
+  }) {
+    assert(orElse != null);
+    if (unableToLocate != null) {
+      return unableToLocate();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object>({
+    @required TResult unexpected(_Unexpected value),
+    @required TResult unableToLocate(_unableToLocate value),
+  }) {
+    assert(unexpected != null);
+    assert(unableToLocate != null);
+    return unableToLocate(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object>({
+    TResult unexpected(_Unexpected value),
+    TResult unableToLocate(_unableToLocate value),
+    @required TResult orElse(),
+  }) {
+    assert(orElse != null);
+    if (unableToLocate != null) {
+      return unableToLocate(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _unableToLocate implements SearchCovidFailure {
+  const factory _unableToLocate() = _$_unableToLocate;
+}
