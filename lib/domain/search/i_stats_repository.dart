87db451c0fba7dea82ac4dfcfba97106140f1215
patrickchow0803 <patrickchow0803@gmail.com>
2021-01-18 12:53:38 -0500

    renamed search_covid_failure to stats_failure and their respective freezed files

---
+++ lib/domain/search/i_stats_repository.dart
@@ -0,0 +1,17 @@
+// I in the class name stands for Interface
+// Treat this abstract class as an Interface.
+
+// Abstract classes can have constants, members, method stubs (methods without a body)
+// whereas interfaces can only have constants and methods stubs.
+import 'package:covid_tracker/domain/search/stats_failure.dart';
+import 'package:dartz/dartz.dart';
+
+// I in the class name stands for Interface
+// Treat this abstract class as an Interface.
+
+// Abstract classes can have constants, members, method stubs (methods without a body)
+// whereas interfaces can only have constants and methods stubs.
+
+abstract class IStatsRepository {
+  // Future<Either<SearchCovidFailure, >>
+}
--- lib/domain/search/search_covid_failure.dart
+++
@@ -1,9 +0,0 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-
-part 'search_covid_failure.freezed.dart';
-
-@freezed
-abstract class SearchCovidFailure with _$SearchCovidFailure {
-  const factory SearchCovidFailure.unexpected() = _Unexpected;
-  const factory SearchCovidFailure.unableToLocate() = _unableToLocate;
-}
