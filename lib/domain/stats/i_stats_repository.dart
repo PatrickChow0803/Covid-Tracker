7249b2a26835abe7d07e16a4ed6ad869878bd566
patrickchow0803 <patrickchow0803@gmail.com>
2021-01-18 17:02:10 -0500

    Added a basic interface repository for covid stats

--- lib/domain/stats/i_stats_repository.dart
+++ lib/domain/stats/i_stats_repository.dart
@@ -1,9 +1,5 @@
-// I in the class name stands for Interface
-// Treat this abstract class as an Interface.
-
-// Abstract classes can have constants, members, method stubs (methods without a body)
-// whereas interfaces can only have constants and methods stubs.
-import 'package:covid_tracker/domain/search/stats_failure.dart';
+import 'package:covid_tracker/domain/stats/stats.dart';
+import 'package:covid_tracker/domain/stats/stats_failure.dart';
 import 'package:dartz/dartz.dart';
 
 // I in the class name stands for Interface
@@ -13,5 +9,7 @@ import 'package:dartz/dartz.dart';
 // whereas interfaces can only have constants and methods stubs.
 
 abstract class IStatsRepository {
-  // Future<Either<SearchCovidFailure, >>
+  Future<Either<StatsFailure, Stats>> getStatsByCountryAndState(String country, String state);
+
+  Future<Either<StatsFailure, Stats>> getStatsByCountry(String country);
 }
